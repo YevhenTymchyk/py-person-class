6ab4d2711f51da5f933374419ba7bacd3c1497ce
Євген <ujinjeka@mail.com>
2026-09-18 22:39:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -12,13 +12,12 @@ def create_person_list(people: list) -> list:
     Person.people.clear()
     person_list = [Person(p["name"], p["age"]) for p in people]
 
-    for i in range(len(people)):
-        person_dict = people[i]
-        person_instance = person_list[i]
+    people_map = Person.people
 
+    for person_dict, person_instance in zip(people, person_list):
         if person_dict.get("wife"):
-            person_instance.wife = Person.people[person_dict["wife"]]
+            person_instance.wife = people_map.get(person_dict["wife"])
         if person_dict.get("husband"):
-            person_instance.husband = Person.people[person_dict["husband"]]
+            person_instance.husband = people_map.get(person_dict["husband"])
 
     return person_list
